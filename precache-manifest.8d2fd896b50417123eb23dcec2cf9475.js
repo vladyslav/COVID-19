763ca7fad901e71f7ad0ec07c3303dcb8822fe9f
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-08-12 21:40:44 +0300

    Updates

---
+++ precache-manifest.8d2fd896b50417123eb23dcec2cf9475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb9c6ccc8c282c64d2563afd1d9a95f",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "5abe4f0263666df4bb03",
+    "url": "/covid-19/static/css/main.28924c1f.chunk.css"
+  },
+  {
+    "revision": "7b82b2b2c53fa0847207",
+    "url": "/covid-19/static/js/2.0a4d6bf6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.0a4d6bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abe4f0263666df4bb03",
+    "url": "/covid-19/static/js/main.36e8d665.chunk.js"
+  },
+  {
+    "revision": "55435a1fff164e4b49f2",
+    "url": "/covid-19/static/js/runtime-main.dbd397e9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
